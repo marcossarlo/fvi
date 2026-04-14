68b5cfd15d24bb7de2a85a7b381f41efda544dee
MarcosSarLo <msl.net@gmail.com>
2026-04-14 18:37:37 -0500

    feat(phd): Implementar el Doctorado en Conservación y Restauración del Medio Natural. - Se automatizo el proceso con un script para extraer toda la información de ese Doctorado de la web actual en Joomla. - Falta verificar toda la informción así como poner imagenes adecuadas al Doctorado.

---
+++ src/data/phd/conservacion-restauracion-medio-natural.ts
@@ -0,0 +1,365 @@
+import type { ProgramCurriculum, ResearchCurriculum } from './sostenibilidad';
+
+export const conservacionRestauracionCurriculum: ProgramCurriculum = {
+    id: 'conservacion-restauracion-medio-natural',
+    courses: [
+    {
+        "title": "CURSO 1: Aproximación al Paisaje",
+        "totalHours": 64,
+        "totalCredits": 4,
+        "modules": [
+            {
+                "title": "Aproximación al paisaje: conceptos, valores y problemática",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Normativa e instrumentos legales aplicados al paisaje",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Métodos de análisis y evaluación visual del paisaje",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Ecología del paisaje perspectivas de evaluación",
+                "hours": 16,
+                "credits": 1
+            }
+        ]
+    },
+    {
+        "title": "CURSO 2: Instrumentos de Ordenación, Planificación y Proyectos en el Paisaje",
+        "totalHours": 64,
+        "totalCredits": 4,
+        "modules": [
+            {
+                "title": "Instrumentos de ordenación del paisaje",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Instrumentos de gestión del paisaje",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Información y participación ciudadana",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Proyectos y actuaciones en el paisaje",
+                "hours": 16,
+                "credits": 1
+            }
+        ]
+    },
+    {
+        "title": "CURSO 3: Modelos en Ecología y Gestión de Recursos Naturales",
+        "totalHours": 64,
+        "totalCredits": 4,
+        "modules": [
+            {
+                "title": "La Dinámica de Sistemas",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Construcción de un Modelo de Simulación",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Creación de Modelos de Simulación Ambiental",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Creación de Modelos de Simulación Social",
+                "hours": 16,
+                "credits": 1
+            }
+        ]
+    },
+    {
+        "title": "CURSO 4: Análisis de biodiversidad para biología de la conservación",
+        "totalHours": 64,
+        "totalCredits": 4,
+        "modules": [
+            {
+                "title": "Biodiversidad y biología de la conservación",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Valoración y medición de la diversidad biológica",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Composición de especies",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Herramientas aplicadas a la conservación de la biodiversidad",
+                "hours": 16,
+                "credits": 1
+            }
+        ]
+    },
+    {
+        "title": "CURSO 5: Medicina de la Conservación: Salud ecológica en práctica",
+        "totalHours": 64,
+        "totalCredits": 4,
+        "modules": [
+            {
+                "title": "El enfoque de la Medicina de la Conservación",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Síndrome de distrés ecológico: pérdida de hábitat e intoxicación global",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Salud de ecosistemas, investigación biomédica y conservación",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Alteración y destrucción de hábitat a gran escala",
+                "hours": 16,
+                "credits": 1
+            }
+        ]
+    },
+    {
+        "title": "CURSO 6: Enfermedades Emergentes y Reemergentes",
+        "totalHours": 64,
+        "totalCredits": 4,
+        "modules": [
+            {
+                "title": "Ecología de enfermedades y su relación con la conservación de especies",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Zoonosis y salud pública",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Herramientas para el monitoreo, predicción y control de enfermedades emergentes y reemergentes",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Ecología de enfermedades emergentes infecciosas, aplicaciones para la conservación y manejo de fauna silvestre",
+                "hours": 16,
+                "credits": 1
+            }
+        ]
+    },
+    {
+        "title": "CURSO 7: Principios de rehabilitación ambiental",
+        "totalHours": 64,
+        "totalCredits": 4,
+        "modules": [
+            {
+                "title": "Introducción al concepto de rehabilitación terrestre",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Fundamentos en ecología e importancia de la diversidad",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Propiedades del suelo",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Efectos físicos de las plantas en sitios degradados",
+                "hours": 16,
+                "credits": 1
+            }
+        ]
+    },
+    {
+        "title": "CURSO 8: Sucesión Ecológica y rehabilitación del ecosistema",
+        "totalHours": 64,
+        "totalCredits": 4,
+        "modules": [
+            {
+                "title": "Sucesiones ecológicas",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Tipo de perturbaciones",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Estudios de impacto ambiental como herramienta preventiva",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Planeamiento de la rehabilitación",
+                "hours": 16,
+                "credits": 1
+            }
+        ]
+    },
+    {
+        "title": "CURSO 9: Métodos de Rehabilitación y Casos de estudio a distintas escalas",
+        "totalHours": 64,
+        "totalCredits": 4,
+        "modules": [
+            {
+                "title": "Metodologías de la rehabilitación ambiental terrestre",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Enfrentando condiciones adversas",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Monitoreo y mantenimiento de sitios rehabilitados",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Estudios de Caso sobre Rehabilitación Ambiental Terrestre",
+                "hours": 16,
+                "credits": 1
+            }
+        ]
+    },
+    {
+        "title": "CURSO 10: Restauración de los componentes del ecosistema",
+        "totalHours": 64,
+        "totalCredits": 4,
+        "modules": [
+            {
+                "title": "La Restauración ecológica y la Restauración ambiental",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Restauración del suelo",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Restauración de la cubierta vegetal",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Restauración de hábitats para fauna",
+                "hours": 16,
+                "credits": 1
+            }
+        ]
+    },
+    {
+        "title": "CURSO 11: Restauración de ecosistemas naturales",
+        "totalHours": 64,
+        "totalCredits": 4,
+        "modules": [
+            {
+                "title": "Restauración de sistemas agrarios",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Restauración de sistemas forestales",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Restauración de zonas áridas",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Restauración de sistemas fluviales y humedales",
+                "hours": 16,
+                "credits": 1
+            }
+        ]
+    },
+    {
+        "title": "CURSO 12: Rehabilitación de áreas degradadas por el hombre",
+        "totalHours": 64,
+        "totalCredits": 4,
+        "modules": [
+            {
+                "title": "La Rehabilitación de espacios urbanos",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Rehabilitación de infraestructuras de comunicación",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Rehabilitación de zonas mineras",
+                "hours": 16,
+                "credits": 1
+            },
+            {
+                "title": "Rehabilitación de vertederos",
+                "hours": 16,
+                "credits": 1
+            }
+        ]
+    }
+]
+};
+
+export const conservacionRestauracionResearch: ResearchCurriculum = {
+    seminars: [
+        {
+            title: 'Seminario de Investigación I: Cómo identificar el problema para una investigación. Estado de la Cuestión',
+            hours: 70,
+            credits: 4,
+        },
+        {
+            title: 'Seminario de Investigación II: Diseño Metodológico',
+            hours: 70,
+            credits: 4,
+        },
+        {
+            title: 'Seminario de Investigación III: Técnicas de investigación, recolección y análisis de datos',
+            hours: 280,
+            credits: 18,
+        },
+        {
+            title: 'Seminario de Investigación IV: Pautas para Estructurar y Redactar la Tesis Doctoral, y el Artículo Científico',
+            hours: 140,
+            credits: 9,
+        },
+    ],
+    phdProject: {
+        title: 'Proyecto de Tesis Doctoral y Artículo Científico',
+        hours: 112,
+        credits: 7,
+    },
+    totalFormation: {
+        hours: 768,
+        credits: 48,
+    },
+};
